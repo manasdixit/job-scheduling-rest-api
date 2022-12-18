4f6f72ee70ada811dbb3b5a95c97508187cbf170
manas.dixit <manas@polyscripts.com>
2022-12-18 15:25:52 +0530

    added database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { Worker } from './worker/entities/worker.entity';
 import { WorkerModule } from './worker/worker.module';
 
 @Module({
-  imports: [UsersModule, WorkerModule],
+  imports: [
+    UsersModule,
+    WorkerModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'library',
+      entities: [User, Worker],
+      synchronize: true,
+      dropSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WorkerService } from './worker.service';
 import { WorkerController } from './worker.controller';
+import { Worker } from './entities/worker.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Worker])],
   controllers: [WorkerController],
-  providers: [WorkerService]
+  providers: [WorkerService],
 })
 export class WorkerModule {}
